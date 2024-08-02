7c8df415639e20455afd8fdf0da2a6f328038a23
Selman Demirdoven <sdemirdoven35@gmail.com>
2024-08-03 02:36:31 +0300

    create files & folders

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import { Tabs, Redirect } from 'expo-router'
+
+const TabsLayout = () => {
+  return (
+    <>
+        <Tabs>
+            <Tabs.Screen name="home" />
+        </Tabs>
+    </>
+  )
+}
+
+export default TabsLayout
+
+const styles = StyleSheet.create({})
---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
+import { Link } from 'expo-router'
+
+const RootLayout = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">gttt oooooo</Text>
+      <StatusBar style="auto" />
+      <Link href="/home" style={{ color: 'blue', }} >Go to Home</Link>
+    </View>
+  )
+}
+
+export default RootLayout
