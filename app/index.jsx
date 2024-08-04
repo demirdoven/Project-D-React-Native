f9b948e72dba1778741a485bf43ac2890cf0c374
Selman Demirdoven <sdemirdoven35@gmail.com>
2024-08-05 01:40:44 +0300

    context +

--- app/index.jsx
+++ app/index.jsx
@@ -5,66 +5,74 @@ import { Redirect, router } from "expo-router";
 import { images } from "../constants";
 import CustomButton from "../components/CustomButton";
 import { StatusBar } from "expo-status-bar";
+import { useGlobalContext } from "../context/GlobalProvider";
 
 const App = () => {
-  
-  return (
-    <SafeAreaView className="bg-primary h-full">
-    
-        <ScrollView
-        contentContainerStyle={{
-            height: "100%",
-        }}
-        >
-            <View className="w-full justify-center items-center min- h-[85vh] px-4">
-                
-                <Image
-                    source={images.logo}
-                    className="w-[130px] h-[84px]"
-                    resizeMode="contain"
-                />
 
-                <Image
-                    source={images.cards}
-                    className="max-w-[380px] w-full h-[298px]"
-                    resizeMode="contain"
-                />
+    const {isLoading, isLoggedIn} = useGlobalContext();
 
-                <View className="relative mt-5">
-                    <Text className="text-3xl text-white font-bold text-center">
-                        Discover Endless{"\n"}
-                        Possibilities with{" "}
-                        <Text className="text-secondary-200">Aora</Text>
-                    </Text>
+    if( ! isLoading && isLoggedIn ) return alert('evet')
+    // <Redirect href="/home" />
 
+    console.log('ddd', isLoggedIn)
+
+    return (
+        <SafeAreaView className="bg-primary h-full">
+
+            <ScrollView
+            contentContainerStyle={{
+                height: "100%",
+            }}
+            >
+                <View className="w-full justify-center items-center min- h-[85vh] px-4">
+                    
                     <Image
-                        source={images.path}
-                        className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+                        source={images.logo}
+                        className="w-[130px] h-[84px]"
                         resizeMode="contain"
                     />
+
+                    <Image
+                        source={images.cards}
+                        className="max-w-[380px] w-full h-[298px]"
+                        resizeMode="contain"
+                    />
+
+                    <View className="relative mt-5">
+                        <Text className="text-3xl text-white font-bold text-center">
+                            Discover Endless{"\n"}
+                            Possibilities with{" "}
+                            <Text className="text-secondary-200">Aora</Text>
+                        </Text>
+
+                        <Image
+                            source={images.path}
+                            className="w-[136px] h-[15px] absolute -bottom-2 -right-8"
+                            resizeMode="contain"
+                        />
+                    </View>
+
+                    <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+                        Where Creativity Meets Innovation: Embark on a Journey of Limitless
+                        Exploration with Aora
+                    </Text>
+
+                    <CustomButton 
+                        title="Continue with email"
+                        handlePress={ () => router.push('/sign-in') }
+                        containerStyles="w-full mt-7"
+                    />
+                
                 </View>
+            </ScrollView>
 
-                <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
-                    Where Creativity Meets Innovation: Embark on a Journey of Limitless
-                    Exploration with Aora
-                </Text>
-
-                <CustomButton 
-                    title="Continue with email"
-                    handlePress={ () => router.push('/sign-in') }
-                    containerStyles="w-full mt-7"
-                />
+            <StatusBar 
+                backgroundColor="#161622"
+                style="light"
+            />
             
-            </View>
-        </ScrollView>
-
-        <StatusBar 
-            backgroundColor="#161622"
-            style="light"
-        />
-        
-    </SafeAreaView>
-  );
+        </SafeAreaView>
+    );
 };
 
 export default App;
